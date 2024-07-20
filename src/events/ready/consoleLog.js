9f6f770fbbb64c9fd27e9bc8b482f7d6cf25016a
ARNTOR131 <petr1223334@gmail.com>
2024-07-20 18:49:35 +0000

    old bugs fixed, new bugs added

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,3 +1,9 @@
-module.exports = (client) => {
+module.exports = async (client) => {
     console.log(`${client.user.tag} is online`);
+
+    await client.channels.fetch(process.env.TEST_CHANNEL_ID)
+    .then(channel => {
+        console.log(channel.name);
+        channel.send('ZOVBOT is online');
+    });
 };
